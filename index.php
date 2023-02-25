09494cb502ac9a8d16a6621c9331d919604386df
Teslacoin <123431268+yusupgaziev@users.noreply.github.com>
2023-02-25 03:25:21 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,460 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Teslacoin</title>
+<meta charset="utf-8">
+
+<meta name="author" content="Teslacoin">
+<meta name="copyright" content="Teslacoin">
+<link rel="shortcut icon" href="icons8-tesla-.png" type="image/png">
+
+<link rel="manifest" href="/lib/js/manifest.json">
+<meta name="theme-color" content="#686de0">
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:locale" content="en_US"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/anim.css?v1"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/dark.css?v7"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/main.css?v1"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/debug.css?v1"/>
+<link rel="stylesheet" type="text/css" href="/lib/css/scale.css?v1"/>
+<script data-cfasync="false" src='/lib/js/jquery.js?v1'></script>
+<script data-cfasync="false" src='/lib/js/dark.js?v5'></script>
+<script data-cfasync="false" src='/lib/js/main.js?v1'></script>
+<script data-cfasync="false" src='/lib/js/jscookie.js?v1'></script>
+<script data-cfasync="false" src='/lib/js/particles.js?v1'></script>
+<script data-cfasync="false" src='/lib/js/particles-sel.js?v1'></script>
+<noscript>
+<style>
+/*
+General:
+- Elements that fade in are not visible by default
+*/
+.fade-up-onstart {
+	display:inherit;
+}
+.fade-in-onload {
+	display:inherit;
+}
+/*
+Index:
+- Center index contents
+*/
+.landing-con-container {
+	top:50%;
+	margin-top:-139px;
+}
+/*
+Quickstart:
+- Remove dimming
+- Do not block interaction outside the sidebar
+- Slightly reduce the width of the sidebar
+*/
+.toggle-navsidebar {
+	background:none;
+	width:auto;
+}
+.sidebar-con-anim {
+	width:300px;
+}
+</style>
+</noscript>
+<script data-cfasync="false" src='/lib/js/carousel.js?v=1'></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6161192934701994" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class='mini-menu-con-dimmer'>
+	<div class='mini-menu-con-container'>
+		<div class='mini-menu-ico-logo'>
+		</div>
+		<div class='mini-menu-tx1-block'>
+			<div class='mini-menu-emp-block'>
+			</div>
+			<h2>No Piracy.</h2>
+		</div>
+		<div class='mini-menu-tx2-block'>
+			<p>
+				 We do not condone piracy of any kind. <u>Asking</u> for, <u>providing</u> or <u>discussing</u> illegal download links is <b>not allowed</b> in our communities.<br>
+				<br>
+				 Purchasing legitimate game copies, through the <u>PlayStation Store</u> or through acquiring game discs, and using those copies with RPCS3 is the best way to ensure you will have a clean copy that will work with the emulator. You can use your <b>legal</b> copies with RPCS3 by following the instructions in our <a href="/quickstart" target="_blank">Quickstart</a> guide.
+			</p>
+		</div>
+		<div class="mini-menu-btn-agree">
+			<h2>I Understand</h2>
+		</div>
+	</div>
+</div>
+<div class='menu-con-message fade-out-onload'>
+	<div class='menu-tx1-message toggle-message fade-out-onload'>
+		<span>We are currently experiencing a higher server volume than usual. We sincerely apologize for any inconveniences.</span>
+	</div>
+</div>
+<div class="window-con-theme">
+	<div class="window-bg-theme">
+	</div>
+	<div class="theme-con-spinner">
+		<div class="theme-ico-spinner">
+		</div>
+	</div>
+	<div class="theme-tx1-description pulsate">
+		<span>This feature is currently not yet available</span>
+	</div>
+	<div class="wavebar-con-container-theme">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+</div>
+<div class="theme-btn-close toggle-theme">
+</div>
+
+<div class="menu-btn-backtotop">
+</div>
+<div class='menu-con-container'>
+	<div class="menu-con-backdrop darkmode-menu">
+	</div>
+	
+	<div class="menu-con-outer">
+		<div class="menu-con-inner">
+			<div class="menu-con-logo">
+				<a href='/'>
+				<div class="menu-ico-logo">
+				</div>
+				</a>
+			</div>
+			<div class="mobile-menu-btn-open toggle-mobilemenu">
+			</div>
+			<div class="mobile-menu-btn-darkmode toggle-darkmode">
+			</div>
+			
+			
+			
+			
+			
+		</div>
+	</div>
+</div>
+<div class='mobile-menu-con-container popup-mobilemenu' style="display:none;">
+	<div class="menu-con-backdrop-2">
+	</div>
+	<div class='mobile-menu-con-scroll'>
+		<div class="mobile-menu-con-outter">
+			<div class="mobile-menu-con-inner" style="overflow: hidden !important;">
+				<div class='mobile-menu-tx1-group'>
+					<span>Main</span>
+				</div>
+				<div class='mobile-menu-con-divider'>
+				</div>
+				<a href="/">
+				<div class='mobile-menu-btn-icon'>
+				</div>
+				<div class='mobile-menu-btn-select'>
+					<span>Home</span>
+				</div>
+				</a>
+				
+
+
+
+
+
+
+
+
+
+<a href="http://twitter.com/teslacoinworld" target="_blank">
+<div class="mobile-menu-btn-icon">
+</div>
+<div class="mobile-menu-btn-select">
+<span>Twitter</span>
+</div>
+</a>
+<a href="http://t.me/teslacoinworld">
+<div class="mobile-menu-btn-icon">
+</div>
+<div class="mobile-menu-btn-select">
+<span>Telegram</span>
+</div>
+</a>
+
+
+
+
+
+
+
+<div class="mobile-menu-btn-spacer">
+</div>
+</div>
+</div>
+		</div>
+	</div>
+</div><script>
+</script>
+<div class="video-con-dimmer toggle-video-1">
+	<div class="menu-btn-close close-video stop-video">
+		<div class="menu-tx1-tooltip">
+			<span>Close</span>
+		</div>
+	</div>
+	<div class="video-con-viewport">
+		<div class="video-img-viewport" style="background: url(/img/videos/1.jpg) no-repeat center; background-size: cover;">
+			<div class="video-ico-viewport"></div>
+		</div>
+		<iframe id="video-1" data-src="https://www.youtube.com/embed/xUpw9bHmoac?autoplay=1&cc_load_policy=1&enablejsapi=1&color=white&version=3&playerapiid=ytplayer" src="about:blank">
+		</iframe>
+	</div>
+</div>
+<script>
+</script>
+<div class="video-con-dimmer toggle-video-2">
+	<div class="menu-btn-close close-video stop-video">
+		<div class="menu-tx1-tooltip">
+			<span>Close</span>
+		</div>
+	</div>
+	<div class="video-con-viewport">
+		<div class="video-img-viewport" style="background: url(/img/videos/2.jpg) no-repeat center; background-size: cover;">
+			<div class="video-ico-viewport"></div>
+		</div>
+		<iframe id="video-2" data-src="https://www.youtube.com/embed/gIcsUr7W00k?autoplay=1&cc_load_policy=1&enablejsapi=1&color=white&version=3&playerapiid=ytplayer" src="about:blank">
+		</iframe>
+	</div>
+</div>
+<script>
+</script>
+<div class="video-con-dimmer toggle-video-3">
+	<div class="menu-btn-close close-video stop-video">
+		<div class="menu-tx1-tooltip">
+			<span>Close</span>
+		</div>
+	</div>
+	<div class="video-con-viewport">
+		<div class="video-img-viewport" style="background: url(/img/videos/3.jpg) no-repeat center; background-size: cover;">
+			<div class="video-ico-viewport"></div>
+		</div>
+		<iframe id="video-3" data-src="https://www.youtube.com/embed/60lq9pXzDic?autoplay=1&cc_load_policy=1&enablejsapi=1&color=white&version=3&playerapiid=ytplayer" src="about:blank">
+		</iframe>
+	</div>
+</div>
+<div class="page-con-content landing-con-override">
+	<div class="wavebar-con-container-master">
+		<div class="wavebar-con-wrap">
+			<div class="wavebar-svg-object">
+			</div>
+			<div class="wavebar-svg-object">
+			</div>
+		</div>
+	</div>
+	<div class='landing-ico-scrolldown'>
+	</div>
+	<div class="landing-con-main">
+		<div id="object-particles">
+		</div>
+		<div class="content-con-backdrop darkmode-header">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class='landing-con-left content-expand '>
+					<div class="landing-con-container scale-content-txt-1 fade-in-onload">
+						
+						<div class='landing-tx1-heading pulsate'>
+							<h1>Teslacoin (TLC)</h1>
+		</div>
+						<div class='landing-tx2-heading'>
+							<p>
+							An open source cryptocurrency named after Tesla.
+							</p>
+							<div class='landing-btn-container'>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	
+	
+		
+	<div class="landing-con-discord darkmode-discord">
+		
+		
+		
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="discord-con-container">
+					<div class="discord-con-logo">
+						<div class="discord-img-logo">
+						</div>
+					</div>
+					<div class="discord-con-mantra">
+						<div class="discord-wrp-mantra scale-content-txt-1">
+							<div class="discord-tx1-mantra darkmode-txt">
+								<h2></h2>Tesla Company
+								<div class="landing-con-divider">
+								</div>
+							</div>
+							<div class="discord-tx2-mantra darkmode-txt">
+								<p>
+								It is much more than just an electric vehicle manufacturer. The brand also specializes in the development of innovative technologies, software for autonomous driving, batteries, energy panels and charging stations.
+								</p>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+	
+	<div class="landing-con-contribute darkmode-github">
+		<div class="github-img-backdrop">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="github-con-container">
+					<div class="github-con-logo">
+						<div class="github-img-logo">
+						</div>
+					</div>
+					<div class="github-con-mantra">
+						<div class="github-wrp-mantra scale-content-txt-1 ">
+							<div class="github-tx1-mantra darkmode-txt">
+								<h2>Tesla name and logo</h2>
+								<div class="landing-con-divider">
+								</div>
+							</div>
+							<div class="github-tx2-mantra darkmode-txt">
+								<p>
+								The name Tesla Corporation bears the name of one of the greatest scientists of the beginning of the last century, Nikola Tesla. He is often referred to as the second Leonardo da Vinci, "Lord of Lightning", "Mysterious Genius" and "Tamer of Light". It was thanks to Tesla's bold ideas that the large-scale electrification of our planet began. Therefore, it is not surprising that the founders of an electric car company decided to name their company after the man who did so much to “open” electricity to the world.
+								</p>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="landing-con-patreon darkmode-feature">
+		<div class="patreon-img-backdrop">
+		</div>
+		<div class="content-con-outside">
+			<div class="content-con-inside">
+				<div class="patreon-con-container">
+					<div class="patreon-con-logo">
+						<div class="patreon-img-logo">
+						</div>
+					</div>
+					<div class="patreon-con-mantra">
+						<div class="patreon-wrp-mantra scale-content-txt-1 ">
+							<div class="patreon-tx1-mantra">
+								<h2>How to buy Teslacoin?</h2>
+								<div class="landing-con-divider" style="background: #ff424d">
+								</div>
+							</div>
+							<div class="patreon-tx2-mantra darkmode-txt">
+								<p>
+								You can buy or sell Teslacoin on PancakeSwap
+								</p>
+							</div>
+							<a href="https://pancakeswap.finance/swap?outputCurrency=0x643AbD4A7163853fbE0da985D0124c95d7fD7052&inputCurrency=BNB" target="_blank">
+							<div class="patreon-btn-button">
+								<div class="patreon-ico-button">
+								</div>
+								<div class="patreon-tx1-button">
+									<span>PancakeSwap</span>
+								</div>
+
+							
+
+							</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+<div class="footer-con-container">
+	<div class="footer-con-overlay darkmode-header">
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class="footer-con-header">
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class="footer-ico-logo darkmode-footer-logo">
+					</div>
+					<table class="footer-table-out">
+					<tr>
+						<td>
+							<div class="footer-table-in">
+								
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Social</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="http://t.me/teslacoinworld" target="_blank">
+										<span>Telegram</span>
+										</a>
+									</div>
+									
+									
+									<div class="footer-tx2-bound">
+										<a href="http://twitter.com/teslacoinworld" target="_blank">
+										<span>Twitter</span>
+										</a>
+									</div>
+									
+								</div>
+							</div>
+						</td>
+						
+					</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class='footer-con-credits'>
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class='footer-con-seprator'>
+					</div>
+					<div class="footer-con-foot">
+						<div class="footer-con-side">
+							<div class="footer-tx1-founder">
+								
+								
+								<span>Website by Teslacoin © 2023</span>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer></div>
+</body>
+</html>
